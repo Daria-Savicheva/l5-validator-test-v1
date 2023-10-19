336e1aa045473ebcc9e645ee2dd6a441c3872eb2
Daria Savicheva <monte111@bk.ru>
2023-10-19 12:16:51 +0300

    add Val

--- src/Validator.js
+++ src/Validator.js
@@ -2,10 +2,12 @@ import ArraySchema from './ArraySchema.js';
 import NumberSchema from './NumberSchema.js';
 
 export default class Validator {
+  // eslint-disable-next-line class-methods-use-this
   number() {
     return new NumberSchema([(value) => typeof value === 'number']);
   }
 
+  // eslint-disable-next-line class-methods-use-this
   array() {
     return new ArraySchema([(item) => Array.isArray(item)]);
   }
